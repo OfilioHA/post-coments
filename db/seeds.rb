94bc55944c41edf943165b257c14df224ea5a333
Ofilio Herdocia Aráuz <herdocia20@gmail.com>
2022-10-08 01:28:40 -0600

    Creados modelos, migraciones y seed. Prueba de PostController

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.destroy_all
+Comment.destroy_all
+
+post1 = Post.create(
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet euismod enim. Morbi id consectetur orci. Proin sit amet nunc faucibus, mollis ligula ac. ",
+    state: 1
+)
+
+coment1 = Comment.create(
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet euismod enim. Morbi id consectetur orci. Proin sit amet nunc faucibus, mollis ligula ac. ",
+    state: 1,
+    post: post1
+)
+
