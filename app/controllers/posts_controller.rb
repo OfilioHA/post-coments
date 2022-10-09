e212c7fc8fe81306a12f2c825daa92b4aeb0628a
Ofilio Herdocia Aráuz <herdocia20@gmail.com>
2022-10-08 21:29:23 -0600

    Haciendo consultas a las relaciones

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all
-        render json: @posts
+        @posts = Post.all.includes(:comments)
+        render :json => @posts, 
+            :include => :comments 
     end
+
+    def show
+        @posts = Post.includes(:comments).find(params[:id]) 
+        render :json => @posts, 
+            :include => :comments 
+    end
+
+    def comments
+        @comments = Post.find(params[:id]).comments
+        render json: @comments
+    end
+
 end
